4b18a830baf96c65373456a150606b82dd4d0d85
Evan Eliason <evaneliason1@gmail.com>
2024-04-14 10:05:19 -0700

    add skeleton to each new  required class

---
+++ src/components/PopupWithForm.js
@@ -0,0 +1,11 @@
+import Popup from "./Popup";
+
+export default class PopupWithForms extends Popup {
+  constructor() {}
+
+  _getInputValues() {}
+
+  setEventListeners() {
+    super.setEventListeners();
+  }
+}
---
+++ src/components/PopupWithImages.js
@@ -0,0 +1,9 @@
+import Popup from "./Popup";
+
+export default class PopupWithImages extends Popup {
+  constructor() {}
+
+  open({ name, link }) {
+    super.open();
+  }
+}
